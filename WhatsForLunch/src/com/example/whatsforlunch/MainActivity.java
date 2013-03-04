14f3d10a113c8417c9413207781f6531d06c1d53
carloszaragoza222 <carloszaragoza222@gmail.com>
2013-03-04 05:34:03 +0000

    Main Activity "New Shopping Trip" and "User Settings" buttons implemented. Slight code cleanup. Inserted primitive comments.

--- WhatsForLunch/src/com/example/whatsforlunch/MainActivity.java
+++ WhatsForLunch/src/com/example/whatsforlunch/MainActivity.java
@@ -1,9 +1,18 @@
+/** What's For Lunch: Main Menu
+ *  Grocery management application
+ *  Keeps track of perishable groceries
+ *  Suggests meals based on current groceries
+ *  Educational tips for better food shopping
+ */
+
 package com.example.whatsforlunch;
 
 import com.example.whatsforlunch.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +28,27 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	//Launches new shopping trip screen
+	//Commented section contains example of way to send an extra message
+	//	to the new activity screen (requires code in other files)
+	public void launchEnterFoods(View view){
+		Intent intent = new Intent(this, Enter_Foods.class);
+		//EditText editText = (EditText) findViewById(R.id.edit_message);
+		//String message = editText.getText().toString();
+		//intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
+	
+	//Launches user settings option screen
+	//Commented section contains example of way to send an extra message
+	//	to the new activity screen (requires code in other files)
+	public void launchSettings(View view){
+		Intent intent = new Intent(this, User_Settings.class);
+		//EditText editText = (EditText) findViewById(R.id.edit_message);
+		//String message = editText.getText().toString();
+		//intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
 
-}//test comment
+}
