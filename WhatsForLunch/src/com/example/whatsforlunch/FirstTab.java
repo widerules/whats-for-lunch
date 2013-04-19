91d4ca4334ec31bfa6b827a23f6bffca949ae275
fordarnett44@gmail.com <fordarnett44@gmail.com@92c38e75-93f9-214a-2d3b-719cd9fe0454>
2013-04-19 17:46:21 +0000

    major changes to enter foods, added tabs, fixed date picker calendar view problem, however bug left with using tab and date picker on same page

---
+++ WhatsForLunch/src/com/example/whatsforlunch/FirstTab.java
@@ -0,0 +1,21 @@
+package com.example.whatsforlunch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class FirstTab extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        TextView textView = new TextView(this);
+        textView.setText("First Tab");
+        setContentView(textView);
+    }
+
+
+}
